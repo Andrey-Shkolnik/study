aae2e8b52fa0c95a1e6d2ee9d7aa516325d08a02
Andrey-Shkolnik <skolnikkrut@gmail.com>
2022-03-26 16:46:55 +0100

    Day 2 of 100 Programming Python course

---
+++ Day 2/2-3.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+max_age=90
+age_left=int(max_age)-int(age)
+days_left=int(age_left)*365
+weeks_left=int(age_left)*52
+month_left=int(age_left)*12
+print (days_left,weeks_left,month_left)
---
+++ Day 2/Project 2.py
@@ -0,0 +1,7 @@
+print ('Welcome to the tip calculator')
+total_bill=float(input('What was the total bill? $'))
+tip=float(input('How much tip would you like to give? 10, 12, or 15? '))
+people=int(input('How many people to split the bill? '))
+pay=(total_bill/people)*(tip/100+1)
+pay=format(pay, '.2f')
+print ('Each person should pay: $'+ pay)
